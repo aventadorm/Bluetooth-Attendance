916cfae6ad406548b869bab4816694726530207b
aventadorm <mayur.phadte@gmail.com>
2015-03-21 17:51:10 +0530

    Added excel conversion, removed SQLite Database for attendance and added email for the spreadsheet file.

--- src/com/cmpna/npl/AttendanceDatabase.java
+++ src/com/cmpna/npl/AttendanceDatabase.java
@@ -5,11 +5,14 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Iterator;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.bluetooth.BluetoothDevice;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -48,18 +51,17 @@ public class AttendanceDatabase extends Activity {
 		int minute = c.get(Calendar.MINUTE);
 		int hour = c.get(Calendar.HOUR_OF_DAY);
 		int date = c.get(Calendar.DATE);
-		int month = c.get(Calendar.MONTH);
+		int month = c.get(Calendar.MONTH) + 1;
 
 		// Convert the time integers to strings
-		String m = String.valueOf(minute);
 		String h = String.valueOf(hour);
+		String m = String.valueOf(minute);
+
 		String d = String.valueOf(date);
 		String mo = String.valueOf(month);
+
 		// create database with time stamp as the name
-		String dbname = "Date:" + d + "//" + mo + "Time:" + h + ":" + m;
-		db = openOrCreateDatabase(dbname, Context.MODE_PRIVATE, null);
-		db.execSQL("CREATE TABLE IF NOT EXISTS attendancetable(rollno INTEGER, attendance INTEGER);");
-		// db.execSQL("insert into "+"attendancetable"+"(rollno,attendance)values(33,0)");
+		String filename = "Date:" + d + "|" + mo + "Time:" + h + ":" + m;
 
 		// Get address array from arraylist of bluetoothdevice objects
 		int j = 0;
@@ -109,40 +111,41 @@ public class AttendanceDatabase extends Activity {
 			}
 		}
 		for (i = 0; i < sizeofdb; i++)
-			showToast(roll[i] + Integer.toString(flag[i]));
+			showToast(roll[i] + ":" + Integer.toString(flag[i]));
 
-		//for (j = 0; j < sizeofdb; j++)
-			//db.execSQL("INSERT INTO attendancetable VALUES('" + roll[j] + "','"
-				//	+ flag[j] + "');");
 		File path = Environment.getExternalStorageDirectory();
-		File file = new File(path, "/"+dbname+".xls");
+		File file = new File(path, "/" + filename + ".xls");
 		try {
 			workbook = Workbook.createWorkbook(file);
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		WritableSheet sheet = workbook.createSheet("First Sheet", 0);
-		Label label = new Label(0, 2, "A label record");
-		try {
-			sheet.addCell(label);
-		} catch (RowsExceededException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-		} catch (WriteException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-		}
+		WritableSheet sheet = workbook.createSheet("Attendance Sheet", 0);
 
-		Number number = new Number(3, 4, 3.1459);
-		try {
-			sheet.addCell(number);
-		} catch (RowsExceededException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (WriteException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		for (j = 0; j < sizeofdb; j++) {
+			Number number = new Number(0, j, roll[j]);
+			try {
+				sheet.addCell(number);
+			} catch (RowsExceededException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (WriteException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		for (j = 0; j < sizeofdb; j++) {
+			Number number = new Number(1, j, flag[j]);
+			try {
+				sheet.addCell(number);
+			} catch (RowsExceededException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (WriteException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 		}
 		// All sheets and cells added. Now write out the workbook
 		try {
@@ -160,42 +163,19 @@ public class AttendanceDatabase extends Activity {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-
-		Cursor e = db.rawQuery("SELECT * FROM attendancetable", null);
-		e.moveToFirst();
-		while (!e.isAfterLast()) {
-			showToast(e.getString(0) + ":" + e.getString(1));
-			e.moveToNext();
-		}
-
-		// String[] dbaddress = { "40B0FA3B51A8", "7AEEBC662872",
-		// "BCF5AC9A9B5F",
-		// "80E650036B1D", "B8EE65AB96BC", "48D705D72C8E" };
-		// Integer[] roll = { 29, 17, 48, 49, 50, 30 };
-		/*
-		 * for(i=0;i<address.length;i++) { for(j=0;j<5;j++) {
-		 * if(address[i]==dbaaddress[j]) { System.out.println(roll[j]); } else {
-		 * System.out.println("student does not belong
-		 */
-	}
-
-	// displays toasts of absent roll numbers
-	public void Absent(View view) {
-		Cursor cu = db.rawQuery(
-				"SELECT rollno FROM attendancetable WHERE attendance = '0'",
-				null);// retrieve student roll nos with attendance marked as
-						// zero
-		// cursor traversal
-		if (!cu.moveToFirst()) {
-			showToast("Everyone is present.");
-		} else {
-			cu.moveToFirst();
-			while (!cu.isAfterLast()) {
-				showToast(cu.getString(0) + " is absent.");
-				cu.moveToNext();
-			}
-		}
-
+		// Uri uri = Uri.fromFile(new
+		// File(Environment.getExternalStorageDirectory(),
+		// filename+".xls"));
+		Uri uri = Uri.fromFile(file);
+		Intent emailintent = new Intent(Intent.ACTION_SEND);
+		emailintent.putExtra(Intent.EXTRA_SUBJECT, "Attendance for " + filename
+				+ ".");
+		emailintent.putExtra(Intent.EXTRA_TEXT, "Attached is attendance for "
+				+ d + "|" + mo + " at time :" + h + ":" + m + ".");
+		emailintent.putExtra(Intent.EXTRA_STREAM, uri);
+		emailintent.setType("text/plain");
+		startActivity(Intent.createChooser(emailintent,
+				"Send attendance as mail"));
 	}
 
 	// function for displaying toast messages
